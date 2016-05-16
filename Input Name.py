888a89b72cfce79fd419154191efbc51f5245411
flydale <flydale5@gmail.com>
2016-05-15 20:33:47 -0500

    Add files via upload

---
+++ Input Name.py
@@ -0,0 +1,10 @@
+"""First Name, Last Name Script.
+
+My first attempt at a Python script
+Dale McPherson 5/15/2016.
+"""
+
+firstname = raw_input('Please type your first name: ')
+lastname = raw_input('please type your last name: ')
+print 'It is very nice to meet you ',firstname,lastname
+
---
+++ Pay Calculator.py
@@ -0,0 +1,9 @@
+"""Pay Calculator Script.
+
+Calculates total pay using hourly rate multiplied by hours worked
+Dale McPherson 5/15/2016.
+"""
+
+hourrate = raw_input('What do you get paid hourly?: ')
+hours = raw_input (How many hours did you work?: ')
+print 'Your pay will be: ' hourrate * hours
